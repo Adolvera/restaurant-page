a83d2f34cb09cd1c7cb0dfed6eae8ceddca6e15d
Adolvera <aolvera4734@gmail.com>
2022-04-22 18:50:55 -0400

    fixed image sources

--- src/menu.js
+++ src/menu.js
@@ -7,12 +7,12 @@ const loadMenu = () => {
   const menuDiv = document.createElement('div');
   const menuGridDiv = document.createElement('div');
 
-  const italianDiv = makeGridItem("../img/italian-sub.jpg", '"Classic Italian"', "Loaded with turkey, pepperoni, salami, and banana peppers");
-  const meatballDiv = makeGridItem("../img/meatball-sub.jpg", '"The Meatball"', "Homemade meatballs flavored with tomato sauce, topped with cheese!");
-  const hoagieDiv = makeGridItem("../img/hoagie-sub.jpg", '"The Hoagie"', "Thinly sliced ham, salami, and Deli American combined with Italian dressing");
-  const phillyDiv = makeGridItem("../img/cheesesteak.jpeg", '"Philly Cheesesteak"', "Ribeye steak, melted provolone, and carmelized onions on a hoagie roll!");
-  const chickenDiv = makeGridItem("../img/chicken-wrap.jpg", "Chicken Wrap", "Choice of fried or grilled chicken, along with choice of vegetables and cheese");
-  const spinachDiv = makeGridItem("../img/spinach-wrap.jpg", "Spinach Wrap", "Turkey spinach wrap, including your choice of vegetables and cheese");
+  const italianDiv = makeGridItem("./img/italian-sub.jpg", '"Classic Italian"', "Loaded with turkey, pepperoni, salami, and banana peppers");
+  const meatballDiv = makeGridItem("./img/meatball-sub.jpg", '"The Meatball"', "Homemade meatballs flavored with tomato sauce, topped with cheese!");
+  const hoagieDiv = makeGridItem("./img/hoagie-sub.jpg", '"The Hoagie"', "Thinly sliced ham, salami, and Deli American combined with Italian dressing");
+  const phillyDiv = makeGridItem("./img/cheesesteak.jpeg", '"Philly Cheesesteak"', "Ribeye steak, melted provolone, and carmelized onions on a hoagie roll!");
+  const chickenDiv = makeGridItem("./img/chicken-wrap.jpg", "Chicken Wrap", "Choice of fried or grilled chicken, along with choice of vegetables and cheese");
+  const spinachDiv = makeGridItem("./img/spinach-wrap.jpg", "Spinach Wrap", "Turkey spinach wrap, including your choice of vegetables and cheese");
 
   mainDiv.id = "main-content";
   mainDiv.classList.add('flex');
